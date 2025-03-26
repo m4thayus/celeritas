718567bcc91cc412a53224d02bba4883ed445711
Matt Williams <matt.seongjun@gmail.com>
2025-03-26 11:38:24 -0400

    init

---
+++ lib/celeritas.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "celeritas/version"
+
+module Celeritas
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/celeritas_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Celeritas do
+  it "has a version number" do
+    expect(Celeritas::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
